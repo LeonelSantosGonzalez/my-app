1b6d78a9a11b2e2e4479eb586292b45c231ae92b
LeonelSantosGonzalez <leonel.sg@ixtapaluca.tecnm.mx>
2023-02-08 19:37:56 -0600

    Nombre del Proyecto: my-app. Descripccion: Version final del proyecto final

---
+++ src/main/java/com/leo/myapp/entity/TestEntity.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.leo.myapp.entity;
+
+/**
+ * @author USERS
+ *
+ */
+public class TestEntity {
+	/*
+	*  Propiedad de prueba para la muestra de compilacion del archivo testEntity 
+ 	*
+	*/
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	private String nombre;
+	
+
+}
